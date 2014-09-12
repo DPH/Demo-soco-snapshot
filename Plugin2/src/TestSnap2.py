42c8dd6ea2108e49b61b80ba80dbff2814f78e3c
David <dh71100@gmail.com>
2014-09-12 08:02:02 +0100

    Latest fixes
    Only one problem left - unicode radio stations!

--- Plugin2/src/TestSnap2.py
+++ Plugin2/src/TestSnap2.py
@@ -13,6 +13,13 @@ alert = "x-file-cifs://DoorPi/DoorPiPublic/doorbell/sounds/Tinkle5sec.mp3"
 # get any Sonos player
 start_zp = soco.SoCo('192.168.1.64')
 
+
+def to_utf8(s):
+    if isinstance(s, unicode):
+        return s.encode('utf-8')
+    else:
+        return s
+
 # take snapshot of current state
 for grp in start_zp.all_groups:
     for zp in grp:
@@ -29,9 +36,15 @@ for grp in start_zp.all_groups:
 
         # FYI get the image saved and print it out (really only for testing)
         img = zp.my_snap.image
+        print img
+
+        #time.sleep(1)
+
         print ('   > Image print for zone: %s' % zp.player_name)
-        for x in img:
-            print ('     %-10s: %s' % (x, img[x]))
+        for x, y in img.iteritems():
+            output = to_utf8(y)
+            #output = output.encode('utf-8')
+            print '     {0: >10}: {1}'.format(x, output)
         print #create blank line
 
 
--- Plugin2/src/soco/plugins/snapshot.py
+++ Plugin2/src/soco/plugins/snapshot.py
@@ -12,8 +12,17 @@ Note:   on a coordinator zone (master) changes what is playing etc.
 
 from __future__ import unicode_literals
 from ..plugins import SoCoPlugin
+from ..utils import really_utf8
+from urllib import unquote
+import logging
 
 __all__ = ['Snapshot']
+
+log = logging.getLogger(__name__)
+logging.basicConfig(format="%(asctime)s %(levelname)-8s-> %(message)s  "
+                           "(%(module)s: %(lineno)s)",
+                    datefmt="%m/%d/%Y %I:%M:%S")
+logging.getLogger(__name__).setLevel(logging.DEBUG)
 
 class Snapshot(SoCoPlugin):
     """
@@ -36,7 +45,8 @@ class Snapshot(SoCoPlugin):
     SOURCE_LIST = {'x-rincon-queue':'queue',
                    'x-file-cifs':'file',
                    'x-rincon':'slave',
-                   '': 'empty'}
+                   'x-sonosapi-stream':'stream',
+                   '': 'no music'}
 
     def __init__(self, soco):
         """ Initialize the plug-in"""
@@ -50,62 +60,60 @@ class Snapshot(SoCoPlugin):
 
     def snap(self):
         """
-        Captures current state of a zone player and
-        store information in an internal dictionary snapped{}
+        Captures current state of a zone player and stores information
+        in an internal dictionary snapped{}
 
         Details captured are those applicable to the state of the zone.
-            always:
+            Always capture:
                 volume = integer
                 mute = True / False
                 media_uri (shows full media source)
-                source (interpretation of media_uri prefix = one of:
-                        'stream', 'queue', 'file', 'slave', 'empty' )
-                        Note: slave means music from coordinator
-                        Note: empty means nothing was playing or paused
+                source - an interpretation of media_uri prefix:
+                    'stream': playing radio or other stream
+                    'queue':  playing form queue
+                    'file':   direct play of file
+                    'slave':  music source is a coordinator zone player
+                    'no music':  nothing was playing or queued
 
-            if playing a stream:
+            stream source capture:
                 media_meta (Current URI MetaData)
 
-            if a coordinator (master) not a slave, also captures:
-                state = PLAYING / STOPPED / TRANSITIONING / PAUSED_PLAYBACK
-
-            if a coordinator playing from queue also capture:
-                ### track_uri (uri of current playing track) - not used!
+            queue source capture:
                 track_no (playlist track number)
                 position (time played)
 
-        """
+            if source is coordinator (master) - not slave:
+                state = PLAYING / STOPPED / TRANSITIONING / PAUSED_PLAYBACK
 
+        """
         self.snapped['mute'] = self.soco.mute
         self.snapped['volume'] = self.soco.volume
 
         media_info = self.soco.avTransport.GetMediaInfo([('InstanceID', 0)])
         self.snapped['media_uri'] = media_info['CurrentURI']
 
-        if self.snapped['media_uri'].split(':')[0] in self.SOURCE_LIST:
-            self.snapped['source'] = self.SOURCE_LIST[
-                                    self.snapped['media_uri'].split(':')[0]]
-        else:
-            # stream most common and not in SORCE_LIST as multiple forms
-            self.snapped['source'] = 'stream'
+        source = self.SOURCE_LIST.get(
+            self.snapped['media_uri'].split(':')[0],'unknown')
+        self.snapped['source'] = source
+
+        if source == 'unknown':
+            log.error('unknown Sonos source: {}'.
+                      format(self.snapped['media_uri'].split(':')[0]))
+
+        if source == 'stream':
             # meta data only used on stream
             self.snapped['media_meta'] = media_info['CurrentURIMetaData']
 
-        if self.snapped['source'] == 'queue':
+        if source == 'queue':
             # only need this if playing from queue
             track_info = self.soco.get_current_track_info()
-
-            # TODO: remove?
-            # self.snapped['track_uri'] = track_info['uri']
-
-            # starts at 1
             self.snapped['track_no'] = track_info['playlist_position']
             self.snapped['position'] = track_info['position']
 
         # only need for master zones
-        if self.snapped['source'] != 'slave':
-            self.snapped['state'] = self.soco.get_current_transport_info(
-                                                )['current_transport_state']
+        if source != 'slave':
+            self.snapped['state'] = self.\
+                soco.get_current_transport_info()['current_transport_state']
 
     def restore(self, fade=False, img=None):
         """
@@ -121,31 +129,33 @@ class Snapshot(SoCoPlugin):
             img = self.snapped
 
         if img['source'] != 'slave':
-            # only for masters (not slaves)
+            # for masters (coordinators) not slaves
 
             # determine what was playing based on source and re-in-state
             if img['source'] in ["queue", "stream"]:
 
-#                 # check if meta data exists - if not use ''
-#                 if 'media_meta' in img:
-#                     meta_data = img['media_meta']
-#                 else:
-#                     meta_data = ''
-
                 # set the URI to the queue or stream that was playing
+                # include meta data if it existed else ''
+
+                #meta = img.get('media_meta', '').encode('utf-8')
+                # print img
+
+                restore_meta = img.get('media_meta', '')
+                # restore_meta = restore_meta #.encode('utf-8')
+                # print type(restore_meta)
+
                 self.soco.avTransport.SetAVTransportURI([
                             ('InstanceID', 0),
                             ('CurrentURI', img['media_uri']),
-                            ('CurrentURIMetaData', img.get('media_meta', ''))
+                            ('CurrentURIMetaData', restore_meta)
                             ])
 
-            if img['source'] == "queue":
+            if img['source'] == "queue" and img['track_no']>'0':
                 # was playing track in queue - must be an integer Track
-                # numbering starts 0!
-                
-                # self.soco.play_from_queue(int(img['track_no']) - 1)
+                # if track number 0 then nothing in queue - so skip
 
                 # set the track number in queue with seek command
+                # use direct call as play_from_queue starts track playing
                 self.soco.avTransport.Seek([
                         ('InstanceID', 0),
                         ('Unit', 'TRACK_NR'),
@@ -160,34 +170,26 @@ class Snapshot(SoCoPlugin):
                         ('Target', ('0' + img['position']))
                         ])
 
-#             elif img['source'] == "stream":
-#                 # was playing stream - resume
-#                 # self.soco.play_uri(img['media_uri'], img['media_meta'])
-#
-#                 # The track is enqueued, now play it.
-#
-# #             if img['state'] != "PLAYING":  # was't Playing
-# #                 # above soco commands start playing so pause it!
-# #                 self.soco.pause()
-
             if img['state'] == "PLAYING":
                 self.soco.play()
 
-
-        # reset mute
+        # --reset mute
         self.soco.mute = img['mute']
 
-        # restore volume (can't change if a fixed_vol zone - so check first!)
-        fixed_vol = False
-        # fixed volume always shows as 100 (but so can real volume) so check
+        # --restore volume
+        # can't change if zone player set to fixed volume so check first
+        # fixed volume always has volume set to 100 (we have the volume stored)
+        # but could be real volume so check (read OutputFixed from zone)
         if img['volume'] == 100:
             fixed_vol = self.soco.renderingControl.GetOutputFixed(
-                              [('InstanceID', 0)])['CurrentFixed']
+                [('InstanceID', 0)])['CurrentFixed']
+        else:
+            fixed_vol = False
+
         if not fixed_vol:
             if fade:
-                # set volume to 0 and fade up
+                # set volume to 0 and fade up (non blocking)
                 self.soco.volume = 0
-                # turn up volume slowly - non blocking
                 self.soco.renderingControl.RampToVolume(
                     [('InstanceID', 0), ('Channel', 'Master'),
                     ('RampType','SLEEP_TIMER_RAMP_TYPE'),
